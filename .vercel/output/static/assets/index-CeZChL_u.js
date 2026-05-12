79ef968712e4c2d95a93ef7d1446e01fd19b1c52
Zafran <zafran@statusmy.com>
2026-05-12 17:58:25 +0800

    chore: exclude .vercel build artifacts

--- .vercel/output/static/assets/index-CeZChL_u.js
+++
@@ -1,1 +0,0 @@
-import{r as i}from"./index-a34zLPwc.js";const t=i("PushNotifications",{});export{t as PushNotifications};
--- .vercel/output/static/assets/web-Du5XYK-2.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./index-a34zLPwc.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
